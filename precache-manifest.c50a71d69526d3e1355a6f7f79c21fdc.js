b99636558eb011f10f51cd9d5c735c73efe7375c
João Amaral <guilive13@gmail.com>
2020-08-08 12:31:28 -0300

    Updates

---
+++ precache-manifest.c50a71d69526d3e1355a6f7f79c21fdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a61ad3c497e01bd80261f93fec2916a",
+    "url": "/force-side/index.html"
+  },
+  {
+    "revision": "3033f8fd478f83ddd571",
+    "url": "/force-side/static/js/2.12e96a0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/force-side/static/js/2.12e96a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b26617262cadc681d28",
+    "url": "/force-side/static/js/main.20cab72b.chunk.js"
+  },
+  {
+    "revision": "b4dae852fb1f49a88653",
+    "url": "/force-side/static/js/runtime-main.9674b7fc.js"
+  },
+  {
+    "revision": "3e06b3a8400a31f968fb37d4a368c9fe",
+    "url": "/force-side/static/media/darth-vader.3e06b3a8.png"
+  },
+  {
+    "revision": "738fc4804a1b605797968b2f1446769f",
+    "url": "/force-side/static/media/luke-skywalker.738fc480.png"
+  }
+]);
